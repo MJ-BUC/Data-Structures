d9fd6cc42b9a8d313233501e640d94ee6c2e2a69
Mark <m.bucaro@outlook.com>
2021-09-10 10:44:19 -0500

    addition of files

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,66 @@
+
+package LinkedList;
+
+public class LinkedList {
+
+private MyNode head;
+}
+
+public int size() {
+    MyNode cursor = head;               //set cursor to head so we can traverse
+    int count = 0;                      //initialize counter to 0
+    while (cursor != null) {            //check to see if we've
+                                        //gotten to the end of the list
+        count++;                        //increment counter
+        cursor = cursor.getNext();      //move cursor through the list
+     }
+        return count;                   //return the count (i.e. size of list)
+}
+
+public boolean isEmpty() {
+    if (size() == 0) {
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+
+public void add() {
+    MyNode cursor = head;
+    int count = 0;
+    while (cursor != null) {
+        count ++;
+        cursor = cursor.getNext();
+    }
+}
+
+public void insert() {
+    MyNode cursor = head;
+    int count = 0;
+    while (cursor != null) {
+        count ++;
+        cursor = cursor.getNext();
+    }
+}
+
+public void delete() {
+    MyNode cursor = head;
+    int count = 0;
+    while (cursor != null) {
+        count ++;
+        cursor = cursor.getNext();
+    }
+}
+
+public void remove() {
+    MyNode cursor = head;
+    int count = 0;
+    while (cursor != null) {
+        count ++;
+        cursor = cursor.getNext();
+    }
+}
+public static void main(String[] args) {
+    
+}
