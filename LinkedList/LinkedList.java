610f5d8a3cb4a5cea5b947f0f6eb873d73e251a9
MJ-BUC <82846057+MJ-BUC@users.noreply.github.com>
2021-09-18 12:21:50 -0500

    addition of more comments

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -31,7 +31,7 @@ public class LinkedList {
         
         MyNode cursor = head;                   //start the cursor at the head
         if (cursor == null) {                   //if the first value is null (meaning Linked list is empty) create head with current node
-            head = node;
+            head = node;                        //sets the node put into the linked list as the head
         }
         else{
 
@@ -46,10 +46,10 @@ public class LinkedList {
         MyNode node = new MyNode(num);            //creates node
 
         MyNode cursor = head;                     //cursor used to traverse to position
-        int count = 0;                            //create counter 
+        int count = 0;                            //create counter to keep track of positions traversed
         if (pos == 0) {                           //checks if the postion is equal to zero to know if it needs to insert at the head
-            node.setNext(cursor);                 
-            head = node;
+            node.setNext(cursor);                 //attaches the node to the node that the cursor is pointing to
+            head = node;                          //sets the node put into the linked list as the head
         }
         else{
         
@@ -68,9 +68,9 @@ public class LinkedList {
         int count = 0;                      //creates counter keeps track of how many times traversed
 
         if (pos == 0) {                             //deleting from the head if position passed is equal to 0
-            cursor.setNext(cursor.getNext());
-            cursor.setNext(null);
-            head = cursor2;
+            cursor.setNext(cursor.getNext());       //sets the next node after the pointer "cursor" to the current spot of pointer "cursor"
+            cursor.setNext(null);                   //sets the node to delete to null
+            head = cursor2;                         //cursor2 is set as the head to replace previous item at position zero
         }
         else{
 
@@ -78,34 +78,33 @@ public class LinkedList {
                 cursor = cursor.getNext();          //traverse the first counter
                 cursor2 = cursor2.getNext();        //traverse counter2 which is one node ahead of first cursor
                 count++;                            //incrementing the counter keeping track of postions traversed
-                if (count == pos - 1) {
-                    cursor.setNext(cursor2.getNext());
-                    cursor2.setNext(null);
+                if (count == pos - 1) {                     //this section runs when cursor2 is on the position that is requested to delete and cursor is on the previous node
+                    cursor.setNext(cursor2.getNext());      //sets the node after desitred position to connect to first cursor
+                    cursor2.setNext(null);                  //sets desired position to null therefore deleting it from the linked list
                 }
             }
         }
     }
 
     public void remove(int num) {                  //add removing at the head
-        MyNode cursor = head;
-        MyNode cursor2 = head.getNext();
-        int value;
-        int count = 0;
+        MyNode cursor = head;                      //creates cursor to point to nodes of linked list
+        MyNode cursor2 = head.getNext();           //creates second cursor with similar functionality to the previous cursor, automatically traverse one ahead of first cursor
+        int value;                                 //saves cursor2 getData() to use in the if statement used for checking if num entered is equal to the getData()
         
-        if (count == 0 && cursor.getData() == num) {
-            cursor.setNext(cursor.getNext());
-            cursor.setNext(null);
-            head = cursor2;
+        if (cursor.getData() == num) {             //when the first cursor is on the desired num this section runs and will only cath the head of the linked list
+            cursor.setNext(cursor.getNext());      //sets the next node after the pointer "cursor" to the current spot of pointer "cursor"
+            cursor.setNext(null);                  //sets the node to delete to null
+            head = cursor2;                        //cursor2 is set as the head to replace previous item at position zero
         }
         else{
             
-            while (cursor2.getData() != num) {
-                cursor = cursor.getNext();
-                cursor2 = cursor2.getNext();
-                value = cursor2.getData();
-                if (value == num) {
-                    cursor.setNext(cursor2.getNext());
-                    cursor2.setNext(null);
+            while (cursor2.getData() != num) {              //runs when cursor2 is on the desired node to remove
+                cursor = cursor.getNext();                  //traverse the first counter
+                cursor2 = cursor2.getNext();                //traverse counter2 which is one node ahead of first cursor
+                value = cursor2.getData();                  //cursor2 gets the data from each node to then store it in the varible value
+                if (value == num) {                         //the data received from the node and stored in value is used to check if that value from the node is equal to num entered
+                    cursor.setNext(cursor2.getNext());      //sets the node after desitred position to connect to first cursor
+                    cursor2.setNext(null);                  //sets desired position to null therefore deleting it from the linked list
                 }
             }
         }
@@ -114,7 +113,7 @@ public class LinkedList {
     public void print() {
         MyNode cursor = head;
         System.out.print("Linked List Items; ");
-        while (cursor != null) {
+        while (cursor != null) {                        //traverse through the entire linked list
             System.out.print(cursor.getData());
             System.out.print(", ");
             cursor = cursor.getNext();
@@ -123,23 +122,24 @@ public class LinkedList {
     public static void main(String[] args) {
         LinkedList mylist = new LinkedList();
         mylist.add(5);                          //adding items to the linked list
-        mylist.add(3);
-        mylist.add(0);
-        mylist.add(7);
-        mylist.add(1);
-        mylist.add(6);
-        mylist.add(4);
-        mylist.add(8);
+        mylist.add(3);                          //-
+        mylist.add(0);                          //-
+        mylist.add(4);                          //-
+        mylist.add(7);                          //-
+        mylist.add(1);                          //-
+        mylist.add(6);                          //-
+        mylist.add(4);                          //-
+        mylist.add(8);                          //adding items to the linked list
 
         mylist.insert(9, 3);                    //inserts 9 at the third position
         mylist.insert(2, 0);                    //inserts 2 at position zero, which is the head
 
-        mylist.remove(8);
-        mylist.remove(4);
-        mylist.remove(2);
+        mylist.remove(8);                       //removes the fist instance of 8 which is the last position
+        mylist.remove(4);                       //removes the first instance of 4 and removes the 4 that comes before the second
+        mylist.remove(2);                       //removes the first instance of 2 which is also the head
 
-        mylist.delete(2);
-        mylist.delete(0);
+        mylist.delete(2);                       //deletes positio 2 of the linked list, 0 is deleted
+        mylist.delete(0);                       //deletes position 0 of the linked list, which is the head and deletes 5
 
         mylist.print();
     }
