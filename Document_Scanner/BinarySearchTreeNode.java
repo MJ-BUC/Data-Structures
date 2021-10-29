081760854c7e9c5ffea8ad2bff29b981d93431e0
MJ-BUC <m.bucaro@outlook.com>
2021-10-29 13:33:27 -0500

    added bracket to node file

--- Document_Scanner/BinarySearchTreeNode.java
+++ Document_Scanner/BinarySearchTreeNode.java
@@ -2,49 +2,50 @@ package CSCE210.Document_Scanner;
 
 public class BinarySearchTreeNode {
 
-public class MyNode {
+	public class MyNode {
 
-	private int nodeData;	//node's data
-	private String nodeKey;
-	private MyNode next;	//node's next pointer
-	private MyNode prev;	//node's prev pointer
+		private int nodeData;	//node's data
+		private String nodeKey;
+		private MyNode next;	//node's next pointer
+		private MyNode prev;	//node's prev pointer
 
-	public MyNode(String key, int value) { 
-		nodeKey = key;
-		nodeData = value;     //initialize node data
-		next = null;		 //initialize next to null
-		prev = null;		 //initialize prev to null
-	}
+		public MyNode(String key, int value) { 
+			nodeKey = key;
+			nodeData = value;     //initialize node data
+			next = null;		 //initialize next to null
+			prev = null;		 //initialize prev to null
+		}
 
-	public String getKey() {	//gets the key from the linked list
-		return nodeKey;
-	}
+		public String getKey() {	//gets the key from the linked list
+			return nodeKey;
+		}
 
-	public int getValue() {
-		return nodeData;	//return node's data
-	}
+		public int getValue() {
+			return nodeData;	//return node's data
+		}
 
-	public void setValue(int value) {
-		nodeData = value;	//set node's data
-	}
+		public void setValue(int value) {
+			nodeData = value;	//set node's data
+		}
 
-	public void setKey(String key) {	//sets the key in the linked list
-		nodeKey = key;
-	}
-	
-	public MyNode getNext() {
-		return next;	//return node's next pointer
-	}
+		public void setKey(String key) {	//sets the key in the linked list
+			nodeKey = key;
+		}
+		
+		public MyNode getNext() {
+			return next;	//return node's next pointer
+		}
 
-	public void setNext(MyNode newnode) {
-		next = newnode;	//set node's next pointer
-	}
+		public void setNext(MyNode newnode) {
+			next = newnode;	//set node's next pointer
+		}
 
-	public MyNode getPrev() {
-		return prev;	//return node's next pointer
-	}
+		public MyNode getPrev() {
+			return prev;	//return node's next pointer
+		}
 
-	public void setPrev(MyNode newnode) {
-		prev = newnode; //set node's next pointer
+		public void setPrev(MyNode newnode) {
+			prev = newnode; //set node's next pointer
+		}
 	}
 }
